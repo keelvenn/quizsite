cb120c377646fe9be79a66b90939b304b4ee1c2d
Kelven <kelvenboss123@gmail.com>
2021-10-09 10:28:22 -0300

    2.0

--- QuizSite/script.js
+++ QuizSite/script.js
@@ -1,22 +1,30 @@
 const perguntas = [{
-  pergunta:"Sexo: ",
-  a: "Eu gosto",
-  b: "Ili, Dili",
+  pergunta:"Qual o maior planeta do sistema solar? ",
+  a: "Júpiter",
+  b: "Sol",
+  c: "Lua",
+  d: "Terra",
   correta: "a"
 },{
   pergunta: "Idade da terra: ",
   a: "4,243 × 5^9",
   b: "4,543 × 10^9",
+  c: "4,543 × 10^10",
+  d: "4,143 × 10^9",
   correta: "b"
 },{
-  pergunta: "Onde mora: ",
-  a: "Na minha casa",
-  b: "No seu coração",
+  pergunta: "Qual a estrela mais velha já conhecida? ",
+  a: "Matusalém",
+  b: "Sua vó",
+  c: "Sol",
+  d: "Cadente",
   correta: "b"
 },{
-  pergunta: "Onde irá morrer? ",
-  a: "Não sei",
-  b: "Em casa",
+  pergunta: "Qual o menor planeta do sistema solar ",
+  a: "Mercúrio",
+  b: "Não sei",
+  c: "Não faço idéia",
+  d: "Júpiter",
   correta: "a"
 }]
 
@@ -25,6 +33,8 @@ const btn = document.getElementById("submit");
 const ques = document.getElementById("q");
 const in_a = document.getElementById("a");
 const in_b = document.getElementById("b");
+const in_c = document.getElementById("c");
+const in_d = document.getElementById("d");
 const l_a = document.getElementById("l_a");
 const l_b = document.getElementById("l_b");
 const inputs = [in_a, in_b];
@@ -34,7 +44,7 @@ let next = 0;
 let at = 0;
 
 init();
-console.log(inputs);
+
 //Funções
 btn.addEventListener("click",function(){
 if(next < perguntas.length){
@@ -52,9 +62,13 @@ function load(){
   ques.innerHTML = quesAt.pergunta;
   l_a.innerHTML = quesAt.a;
   l_b.innerHTML = quesAt.b;
+  l_c.innerHTML = quesAt.c;
+  l_d.innerHTML = quesAt.d;
   //Reset
   in_a.style.display = "flex";
   in_b.style.display = "flex";
+  in_c.style.display = "flex";
+  in_d.style.display = "flex";
   btn.innerHTML = "Próxima"
   console.log("Valor score: " +score ,"Valor At: " + at)
 }//Fim Função
@@ -80,6 +94,24 @@ function controler(){
     at++;
   }
 
+  if(in_c.checked && in_c.value == quesAt.correta){
+    score++;
+    at++
+  }
+
+  if(in_c.checked && in_c.value != quesAt.correta){
+    at++;
+  }
+
+  if(in_d.checked && in_d.value == quesAt.correta){
+    score++;
+    at++
+  }
+
+  if(in_d.checked && in_d.value != quesAt.correta){
+    at++;
+  }
+
 
 }//Fim Função
 
@@ -88,8 +120,12 @@ function init(){
   ques.innerHTML = "Vamos começar!"
   l_a.innerHTML = "";
   l_b.innerHTML = "";
+  l_c.innerHTML = "";
+  l_d.innerHTML = "";
   in_a.style.display = "none";
   in_b.style.display = "none";
+  in_c.style.display = "none";
+  in_d.style.display = "none";
   btn.innerHTML = "Vamos lá!"
 }
 
